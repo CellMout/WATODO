44521c9d74d69b81b3b2aa396268ac0bd8642f2f
benjamin masson <benjamin.masson@gmail.com>
2024-12-10 11:49:19 +0100

    seeds OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Activity.delete_all
+Request.delete_all
+User.delete_all
+
+#seeds users
+User.create(email: "admin@gmail.com", password: "password")
+
+users = []
+2.times do
+  users << User.create!(
+  email: Faker::Internet.unique.email,
+  password: Faker::Internet.password(min_length: 8)
+  )
+end
+
+#seeds requests
+requests = []
+5.times do
+  requests << Request.create!(
+    user: users.sample,
+    duration: rand(1..10),
+    lat: Faker::Address.latitude,
+    lon: Faker::Address.longitude,
+    options: Faker::Lorem.words(number: 3).join(", "),
+    category: Faker::Hobby.activity
+  )
+end
+
+#seeds activities
+5.times do
+  Activity.create!(
+    name: Faker::Lorem.sentence(word_count: 3),
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    location: Faker::Address.city,
+    request: requests.sample
+  )
+end
+
+puts "Created #{User.count} users, #{Request.count} requests, and #{Activity.count} activities."
