0361f1e88041df09ffdc6b3d9c40d1e8a91acf88
Jean Loup <doc.jprs@gmail.com>
2024-12-11 14:26:05 +0100

    fix: remove bad import from manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link controllers/application.js
-//= link controllers/get_user_position_controller.js
 //= link controllers/index.js
 //= link application.js
 //= link bootstrap.min.js
