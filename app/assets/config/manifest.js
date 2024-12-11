4ee9c0dc4d1cadd4ad217f4b6e7d9eaa44c08d00
Elie Celka <elie.celka@gmail.com>
2024-12-11 17:13:06 +0100

    use coords geo loc

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link controllers/application.js
-//= link controllers/get_user_position_controller.js
 //= link controllers/index.js
 //= link application.js
 //= link bootstrap.min.js
 //= link popper.js
+//= link controllers/user_coordinates_controller.js
