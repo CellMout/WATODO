d494ef0c03b6ddbbd57b30ca3daec81e4cbf88ad
benjamin masson <benjamin.masson@gmail.com>
2024-12-11 14:27:57 +0100

    almost

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,15 @@
 class ActivitiesController < ApplicationController
   def update
   end
+
+  def index
+  @activities = Activity.all
+    @markers = @activity.geocoded.map do |activity|
+      {
+        lat: Geocoder.search(@activity.location).first.coordinates[0],
+        lng: Geocoder.search(@activity.location).first.coordinates[1],
+        info_window_html: render_to_string(partial: "info_window", locals: {activity: activity})
+      }
+    end
+  end
 end
